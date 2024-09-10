cd8c3047be99d2585b200356486d71d38ee727bb
Chintan Patel <chintankp15@gmail.com>
2024-09-10 17:02:42 +0530

    First Assignment

---
+++ Programs/Assignments/Assignment_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //  Assignment - 1 : Printing Story on console
+
+    cout << "----------  Fates Entwined  ----------\n\n";
+
+    cout << "Two siblings, Aether and Lumine, are the travellers who roam around the galaxy world by world. They both have came upon a pretty dense situation when they reached the world of Teyvat.\n\n";
+
+    cout << "The God of Heavenly Principles, had came before this siblings while they were about to leave Teyvat. The God suddenly grabbed the Aether (older sibling) and captured him, and left Lumine (younger sibling) powerless to fight.\n\n";
+
+    cout << "Now Traveller, after 500 years, the time has come for you who has been slept through all this years to wake up and to defeat the God of Heavenly Principles and to free your sibling.";
+}
