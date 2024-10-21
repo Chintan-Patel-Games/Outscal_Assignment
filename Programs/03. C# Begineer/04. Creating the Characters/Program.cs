2e441d7cfc9781ef6620f5164c602c783f237c7d
ChintanPatel024 <chintankp15@gmail.com>
2024-10-21 12:01:05 +0530

    Added C# Begineer 04. Assignment-1

---
+++ Programs/03. C# Begineer/04. Creating the Characters/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n==================================================");
+        Console.WriteLine("             🍕 MIDNIGHT PIZZA FIGHT 🍕           ");
+        Console.WriteLine("==================================================");
+        Console.WriteLine("\nIn a bustling pizzaria on a busy Friday night...");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("You, the Dough Master, created your Magnum opus -");
+        Console.WriteLine("the perfect pizza🤌  Suddenly, a Sneaky Crust Bandit");
+        Console.WriteLine("snatches your masterpiece!");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("\nFueled by passion for your craft, you give chase...");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("Through winding alleys and crowded streets, you");
+        Console.WriteLine("pursue the pizza pilferer. Finnaly, the thief is");
+        Console.WriteLine("cornered in a dead-end alley. It's time to recover");
+        Console.WriteLine("your stoler slice!");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("                      FIGHT!\n");
+        Player player = new Player();
+    }
+}
+
+class Player
+{
+    //variables
+    private int health = 100;
+    private int maxHealth = 100;
+    private int attackDamage = 20;
+    private int healingCapacity = 15;
+
+    public int Health
+    {
+        //GETTER
+        get
+        {
+            return health;
+        }
+        //SETTER
+        private set
+        {
+            // If the value provided is negative, store zero instead
+            if (value < 0)
+                health = 0;
+            // if the value exceeds maximum health
+            else if (value > maxHealth)
+            {
+                health = maxHealth;
+            }
+            //set the provided value if both the conditions are false
+            else
+            {
+                health = value;
+            }
+        }
+    }
+
+    //Default Constructor
+    public Player()
+    {
+        SpawnPlayer();
+    }
+
+    //Function
+    private void SpawnPlayer()
+    {
+        Console.WriteLine("\n==================================================");
+        Console.WriteLine(" 🍕 DOUGH MASTER: GUARDIAN OF THE GOLDEN CRUST 🍕");
+        Console.WriteLine("==================================================\n");
+        Console.WriteLine("\nDough Master: That scoundrel won't escape with my creation!");
+    }
+}
