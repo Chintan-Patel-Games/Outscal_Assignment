70c6e01122977e192bcd492e586e5ebc05ff42f6
ChintanPatel024 <chintankp15@gmail.com>
2024-10-21 19:04:11 +0530

    Added C# Begineer 06. Assignment-1 Create Player Combat

--- Programs/Games/02. Midnight Pizza Fight/Midnight_Pizza_Fight/Player.cs
+++ Programs/Games/02. Midnight Pizza Fight/Midnight_Pizza_Fight/Player.cs
@@ -50,5 +50,69 @@ namespace Midnight_Pizza_Fight
             Console.WriteLine("==================================================\n");
             Console.WriteLine("\nDough Master: That scoundrel won't escape with my creation!");
         }
+
+        private int GenerateRandomNumberInRange(int min, int max)
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(min, max + 1);
+            return randomNumber;
+        }
+        
+        public void TakeDamage(int damageRecieved) => Health -= damageRecieved;
+
+        public int CalculateTotalDamage()
+        {
+            int additionalDamage = GenerateRandomNumberInRange(5, 15);
+            int totalDamage = attackDamage + additionalDamage;
+            return totalDamage;
+        }
+
+        public void ShowAttackDamage(int totalDamage)
+        {
+            Console.WriteLine("             🍕 PIZZA BATTLE 🍕                   ");
+            Console.WriteLine("============================================");
+            Console.WriteLine("Dough Master's attack dealt " + totalDamage + " damage! 🥊");
+            Console.WriteLine("--------------------------------------------");
+        }
+
+        public void Heal(int healAmount) => Health += healAmount;
+
+        public int CalculateTotalHeal()
+        {
+            int additionalHeal = GenerateRandomNumberInRange(10, 20);
+            int totalHeal = healingCapacity + additionalHeal;
+
+            return totalHeal;
+        }
+
+        public void ShowHeal(int healAmount)
+        {
+            if (Health >= maxHealth)
+            {
+                Console.WriteLine("             🍕 PIZZA BATTLE 🍕                   ");
+                Console.WriteLine("============================================");
+                Console.WriteLine("     Dough Master is bursting with energy! 🚀    ");
+                Console.WriteLine("--------------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("             🍕 PIZZA BATTLE 🍕                   ");
+                Console.WriteLine("============================================");
+                Console.WriteLine("Dough Master's heal restored " + healAmount + " hp! ☕");
+                Console.WriteLine("--------------------------------------------");
+            }
+        }
+
+        public void DisplayPlayerStats()
+        {
+            Console.WriteLine("\n---------------------------------------------------");
+            Console.WriteLine("              DOUGH MASTER'S STATS                ");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("Health: " + Health + "/" + maxHealth);
+            Console.WriteLine("Dough Slapper: " + attackDamage);
+            Console.WriteLine("Espresso Shot ☕: " + healingCapacity);
+            Console.WriteLine("Dough Slapper Boost 🌪️: 5 to 15");
+            Console.WriteLine("Espresso Shot Boost ☕: 10 to 20");
+        }
     }
 }
