ebc32b16104960e91f2cfba4742e8c44783da972
ChintanPatel024 <chintankp15@gmail.com>
2024-10-21 12:10:12 +0530

    Added new Game Midnight_Pizza_Fight with lore

---
+++ Programs/Games/02. Midnight Pizza Fight/Midnight_Pizza_Fight/Midnight_Pizza_Fight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Midnight_Pizza_Fight
+{
+    static void Main()
+    {
+        Console.WriteLine("\n==================================================");
+        Console.WriteLine("             🍕 MIDNIGHT PIZZA FIGHT 🍕           ");
+        Console.WriteLine("==================================================");
+        Console.WriteLine("\nIn a bustling pizzaria on a busy Friday night...");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("You, the Dough Master, created your Magnum opus -");
+        Console.WriteLine("the perfect pizza🤌  Suddenly, a Sneaky Crust Bandit");
+        Console.WriteLine("snatches your masterpiece!");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("\nFueled by passion for your craft, you give chase...");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("Through winding alleys and crowded streets, you");
+        Console.WriteLine("pursue the pizza pilferer. Finnaly, the thief is");
+        Console.WriteLine("cornered in a dead-end alley. It's time to recover");
+        Console.WriteLine("your stoler slice!");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("                      FIGHT!                      ");
+    }
+}
