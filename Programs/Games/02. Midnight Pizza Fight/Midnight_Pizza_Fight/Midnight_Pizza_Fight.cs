2da907494d69baef962a53958f05e0d35033273e
ChintanPatel024 <chintankp15@gmail.com>
2024-10-21 12:14:11 +0530

    Added Player class in Midnight_Pizza_Fight

--- Programs/Games/02. Midnight Pizza Fight/Midnight_Pizza_Fight/Midnight_Pizza_Fight.cs
+++ Programs/Games/02. Midnight Pizza Fight/Midnight_Pizza_Fight/Midnight_Pizza_Fight.cs
@@ -2,7 +2,7 @@
 
 class Midnight_Pizza_Fight
 {
-    static void Main()
+    static void Main(string[] args)
     {
         Console.WriteLine("\n==================================================");
         Console.WriteLine("             🍕 MIDNIGHT PIZZA FIGHT 🍕           ");
@@ -20,6 +20,57 @@ class Midnight_Pizza_Fight
         Console.WriteLine("cornered in a dead-end alley. It's time to recover");
         Console.WriteLine("your stoler slice!");
         Console.WriteLine("--------------------------------------------------");
-        Console.WriteLine("                      FIGHT!                      ");
+        Console.WriteLine("                      FIGHT!\n");
+        Player player = new Player();
+    }
+}
+
+class Player
+{
+    //variables
+    private int health = 100;
+    private int maxHealth = 100;
+    private int attackDamage = 20;
+    private int healingCapacity = 15;
+
+    public int Health
+    {
+        //GETTER
+        get
+        {
+            return health;
+        }
+        //SETTER
+        private set
+        {
+            // If the value provided is negative, store zero instead
+            if (value < 0)
+                health = 0;
+            // if the value exceeds maximum health
+            else if (value > maxHealth)
+            {
+                health = maxHealth;
+            }
+            //set the provided value if both the conditions are false
+            else
+            {
+                health = value;
+            }
+        }
+    }
+
+    //Default Constructor
+    public Player()
+    {
+        SpawnPlayer();
+    }
+
+    //Function
+    private void SpawnPlayer()
+    {
+        Console.WriteLine("\n==================================================");
+        Console.WriteLine(" 🍕 DOUGH MASTER: GUARDIAN OF THE GOLDEN CRUST 🍕");
+        Console.WriteLine("==================================================\n");
+        Console.WriteLine("\nDough Master: That scoundrel won't escape with my creation!");
     }
 }
