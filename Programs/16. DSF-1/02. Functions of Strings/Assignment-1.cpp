5b0655edb0d3d1cef6839e5dda14bce562e42cba
Chintan Patel <chintanpatelit024@gmail.com>
2025-04-10 10:51:36 +0530

    Added: 16.DSF-1/02.Functions of Strings/Asignment-1

---
+++ Programs/16. DSF-1/02. Functions of Strings/Assignment-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter the string: ";
+    cin >> str;
+
+    unordered_map<char, int> freq;
+    for (char ch : str) {
+        freq[ch]++;
+    }
+
+    string result = "";
+    for (auto pair : freq) {
+        if (pair.second > 1) {
+            result += pair.first;
+        }
+    }
+
+    // Optional: Sort the result if you want it in a consistent order
+    sort(result.begin(), result.end());
+
+    cout << "Duplicate characters: " << result << endl;
+
+    return 0;
+}
